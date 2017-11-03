cf0d1544ffd7d8494c81ad8148ccd65931034b01
Flo Barriol <florent.barriol@gmail.com>
2017-11-03 15:55:02 +0100

    feature: add aboutpage route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, IndexRoute, Redirect } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
+import AboutPage from './components/AboutPage';
 import pokemonFeature from './features/pokemon';
 
 export default (
     <Route path="/" component={App}>
     <IndexRoute component={Home}/>
+    <Route path="about" component={AboutPage}/>
         <Route
             path="pokemon/:id"
             component={pokemonFeature.components.PokemonPage} />
