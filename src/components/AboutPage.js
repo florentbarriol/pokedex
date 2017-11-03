667fada5282ca25c030da22dde277b72464848db
Flo Barriol <florent.barriol@gmail.com>
2017-11-03 15:54:30 +0100

    feature: add about page but with no text

---
+++ src/components/AboutPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AboutPage extends Component {
+
+    render() {
+        return (
+            <main>
+                <h1>About</h1>
+                <p>
+                    to write...
+                </p>
+            </main>
+        );
+    }
+}
+
+export default AboutPage;
